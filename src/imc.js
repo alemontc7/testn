dfbeab2834115ac239616c648ff74af4a52c7a04
Alejandro Montero <alejandro.montero@ucb.edu.bo>
2024-12-11 21:09:49 -0400

    Primer test de calculadora IMC - test fallido

---
+++ src/imc.js
@@ -0,0 +1,5 @@
+function calcularIMC(peso, altura) {
+    // Por ahora, dejamos la función vacía para que falle el test
+  }
+  
+  export default calcularIMC;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,6 +2,14 @@ import sumar from "./sumador.js";
 import restar from "./restar.js"
 import multiplicar from "./multiplicar.js";
 
+import calcularIMC from './imc.js';
+
+describe('Calculadora de IMC', () => {
+  it('debería calcular el IMC correctamente para un caso básico', () => {
+    expect(calcularIMC(70, 1.75)).toBeCloseTo(22.86, 2);
+  });
+});
+
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2, 1)).toEqual(6);
