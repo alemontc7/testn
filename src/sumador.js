da01d7f10d7836f0b628ea4a61e1d186ab7bb978
Alejandro Montero <alejandro.montero@ucb.edu.bo>
2024-12-11 20:56:04 -0400

    no se

--- src/sumador.js
+++ src/sumador.js
@@ -2,4 +2,5 @@ function sumar(a, b, c) {
   return a + b + c;
 }
 
+
 export default sumar;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,8 +1,15 @@
 import sumar from "./sumador.js";
+import restar from "./restar.js"
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2, 1)).toEqual(6);
+  });
+});
+
+describe("Restar", () => {
+  it("deberia Restar dos numeros", () => {
+    expect(restar(3,2)).toEqual(1);
   });
 });
 
